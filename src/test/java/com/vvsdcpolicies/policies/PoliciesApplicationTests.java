f860607830a1362258206bb8ed18e918c45bfdf3
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-09-07 23:03:30 +0530

    Created base folder

---
+++ src/test/java/com/vvsdcpolicies/policies/PoliciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvsdcpolicies.policies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
