9dc47e987d58f5b2894bdfcf156f5fad7a91869c
VVSD-Charan <20bcs244@iiitdmj.ac.in>
2024-09-08 17:15:14 +0530

    Added pmd configuration,API and test case

--- src/test/java/com/vvsdcpolicies/policies/PoliciesApplicationTests.java
+++ src/test/java/com/vvsdcpolicies/policies/PoliciesApplicationTests.java
@@ -1,13 +1,33 @@
 package com.vvsdcpolicies.policies;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.vvsdcpolicies.policies.dto.PoliciesDTO;
+import com.vvsdcpolicies.policies.entity.Policies;
+import com.vvsdcpolicies.policies.repository.PoliciesRepository;
+import com.vvsdcpolicies.policies.service.PoliciesServiceImpl;
 
 @SpringBootTest
 class PoliciesApplicationTests {
+	
+	@Mock
+	private PoliciesRepository policiesRepository;
+	
+	@InjectMocks
+	private PoliciesServiceImpl policiesService;
 
 	@Test
-	void contextLoads() {
+	public void addPolicyValidTest()
+	{
+		PoliciesDTO policiesDTO = new PoliciesDTO();
+		Policies policies = new Policies();
+		
+		Mockito.when(policiesRepository.save(policies)).thenReturn(policies);
+		Assertions.assertDoesNotThrow(()-> policiesService.addPolicy(policiesDTO));
 	}
-
 }
